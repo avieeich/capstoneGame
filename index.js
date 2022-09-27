f679ded758041b1d7bfcac793662baa34e171064
Avie <avie.eich@gmail.com>
2022-09-27 16:25:32 -0600

    basic movement, scroll, platform, collision

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const context = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-console.log(context);
+//ads on to the velocity pushing *see the update function
+const gravity = 1;
 
 // creates Player object, this is the character we want to control
 class Player {
@@ -15,8 +16,14 @@ class Player {
             x: 100,
             y: 100
         }
+        //player is now endangered by the laws of physics (It's part of gravity)
+        this.velocity = {
+            x: 0,
+            y: 0
+        }
         this.width = 30
         this.height = 30
+
     }
     // this creates our character. Who is a red square right now. 
     draw(){
@@ -24,7 +31,126 @@ class Player {
         context.fillRect(this.position.x, this.position.y, this.width, this.height)
         
     }
+    //puts velocity into position.
+update(){
+    this.position.y += this.velocity.y
+    this.position.x += this.velocity.x
+    this.draw()
+
+    if (this.position.y +this.height + this.velocity.y <= canvas.height){
+    this.velocity.y += gravity;} else{ this.velocity.y=0}
 }
+
+}
+//  platform class
+class Platform{
+constructor(){
+    this.position ={
+        x:200, 
+        y:100
+    }
+    this.width = 200
+    this.height = 20
+}
+
+draw() {
+    context.fillStyle = "blue"
+    context.fillRect(this.position.x, this.position.y, this.width, this.height)
+}
+}
+
+
 //initiates our player object
 const player = new Player();
+const platform = new Platform();
+
+const keys = {
+    right: {
+        pressed: false
+    },
+    left: {
+        pressed: false
+    }
+}
 player.draw();
+
+function animate(){
+    requestAnimationFrame(animate)
+    context.clearRect(0, 0, canvas.width, canvas.height)
+    player.update()
+    platform.draw()
+
+    if (keys.right.pressed && player.position.x <400){
+        player.velocity.x = 5
+    } else if (keys.left.pressed && player.position.x > 100){
+        player.velocity.x = -5;
+    } else{
+        player.velocity.x=0
+
+        if(keys.right.pressed){
+            platform.position.x -=5
+        } else if (keys.left.pressed){
+            platform.position.x +=5
+        }
+    }
+
+    if (player.position.y + player.height<= platform.position.y && player.position.y + player.height + player.velocity.y >= platform.position.y && player.position.x + player.width >=platform.position.x && player.position.x <= platform.position.x + platform.width){
+        player.velocity.y=0
+    }
+}
+
+// calls animation function
+animate()
+
+
+
+// Keycodes
+// A: 65, S: 83, D:68, W:87
+
+//event listeners 
+addEventListener("keydown", ({keyCode}) => {
+    switch (keyCode) {
+        case 65:
+            console.log ('left')
+            keys.left.pressed=true
+            break
+
+            case 83:
+            console.log ('down')
+            break
+
+            case 68:
+            console.log ('right')
+            keys.right.pressed=true
+            break
+            
+            case 87:
+            console.log ('up')
+            player.velocity.y -= 20
+            break
+    }
+})
+
+addEventListener("keyup", ({keyCode}) => {
+    switch (keyCode) {
+        case 65:
+            console.log ('left')
+            keys.left.pressed=false
+            break
+
+            case 83:
+            console.log ('down')
+            break
+
+            case 68:
+            console.log ('right')
+            keys.right.pressed=false
+            break
+            
+            case 87:
+            console.log ('up')
+            player.velocity.y -= 20
+            break
+    }
+
+})
